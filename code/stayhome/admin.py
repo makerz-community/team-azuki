f487d661841e46f6e9ed02a18a761edc176f3bd8
hide410 <hsuzuki410@gmail.com>
2020-04-15 23:37:39 +0900

    [add]トップのリストビュー（デザインなし）とboto3の試し
    お試しビューは諸々確認したら削除します。あと、とりあえず_base.htmlでBootstrapを入れ始めてみました。トップのリストビューのフロントはまだいじってません

--- code/stayhome/admin.py
+++ code/stayhome/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import User, Cards
+from .models import User, Cards, HashTags
 
 admin.site.register(User)
 admin.site.register(Cards)
+admin.site.register(HashTags)
--- code/stayhome/urls.py
+++ code/stayhome/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from.views import TopView, CardCreateView, CardPreviewView, TwitterShareView
+from.views import TopView, CardCreateView, CardPreviewView, TwitterShareView, OtameshiView
 
 app_name = 'stayhome'
 urlpatterns = [
@@ -8,4 +8,5 @@ urlpatterns = [
     path('input/', CardCreateView.as_view(), name="input"),
     path('output/', CardPreviewView.as_view(), name="preview"),
     path('share/', TwitterShareView.as_view(), name="share"),
+    path('otameshi/', OtameshiView.as_view(), name="otameshi")
 ]
