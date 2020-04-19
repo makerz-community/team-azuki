217f6daa976aac8f3851f3da0b634c9a7fa18640
mahirusan <mahirusan564@gmail.com>
2020-04-19 14:22:01 +0000

    ハッシュタグ作成or使用処理追加+フォーム全般改善

--- code/stayhome/forms.py
+++ code/stayhome/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import User,Card,HashTags
 
-class CardCreateForm(forms.ModelForm):
-    class Meta:
-        model = Card
-        fields = ('title','context','meeting_link','started_at','stopped_at','target_day','member_number')
+# ModelForm使わずにFormでhash_tagsも一緒にとるように変えました
+class CardCreateForm(forms.Form):
+    title = forms.CharField(label='募集タイトル', max_length=32,widget=forms.TextInput)
+    content = forms.CharField(label='募集内容', max_length=255,widget=forms.Textarea)
+    meeting_link = forms.CharField(
+        label='リンク', max_length=255,widget=forms.TextInput)
+    started_at = forms.TimeField(label='開始時間',widget=forms.TextInput)
+    stopped_at = forms.TimeField(label='終了時間',widget=forms.TextInput)
+    target_day = forms.DateField(label='対象日',widget=forms.Dateinput)
+    member_number = forms.IntegerField(label='予定人数',widget=forms.NumberInput)
+    hash_tags = forms.CharField(label='ハッシュタグ',max_length=255,widget=forms,TextInput)
--- code/stayhome/views.py
+++ code/stayhome/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect,get_object_or_404
 from django.urls import reverse,reverse_lazy
 from django.views import generic
 from .forms import CardCreateForm
+from .models import Cards,HashTags,User
 
 
 ##############
@@ -22,16 +23,29 @@ class CardCreateView(generic.FormView):
     success_url = reverse_lazy('stayhome:preview')
 
     def form_valid(self,form):
-        unfinished_form = form.save(commit=False)
-        unfinished_form.author = self.request.user
-        unfinished_form.conditions = 1
-        unfinished_form.save()
+        # ハッシュタグはユニークで保存したいので取得できなかったら作るようにする
+        try:
+            hashtag = HashTags.objects.get(name=form.cleaned_data['hash_tags'])
+        except:
+            hashtag = HashTags.objects.create(name=form.cleaned_data['hash_tags'])
+        # リクエスト中のユーザーをいれる(twitterとの連携をまた考える)
+        author = self.request.user
+        # 状態：1>募集中、2>募集終了
+        conditions = 1
+        # 募集カードを作る
+        new_card = Cards.objects.create(author=author,title=form.cleaned_data['title'],content=form.cleaned_data['content'],
+        conditions=conditions,meeting_link=form.cleaned_data['meeting_link'],started_at=form.cleaned_data['started_at'],stopped_at=form.cleaned_data['stopped_at'],
+        target_day=form.cleaned_data['target_day'],member_number=form.cleaned_data['member_number'])
+        # 募集カードにさっき取得(作成)したハッシュタグを加えて保存
+        new_card.hash_tags.add(hashtag)
+        new_card.save()
+        # succes_urlへ
         return super().form_valid(form)
 
     def get_initial(self):
         initial = super().get_initial()
         initial["initial_title"] = "募集タイトルを入力してください"
-        initial["initial_content"] = "例：エンジニアが集まってワイワイする予定です"
+        initial["initial_content"] = "例：エンジニアが集まってワイワイする予定です。お酒飲めない人もたくさん参加するので気軽に応募してください。"
         return initial
 
 
