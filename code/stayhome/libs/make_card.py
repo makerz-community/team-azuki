01d483efc505e5bcc56186d65a88a8fe30160fb2
hide410 <hsuzuki410@gmail.com>
2020-05-10 13:20:40 +0900

    画像生成スクリプトだいたいできた
    - あとはformsとviewsができたらviewsに入れ込む
    - デザインが決まったら文字の配置とか変更する

--- code/stayhome/libs/make_card.py
+++ code/stayhome/libs/make_card.py
@@ -1,10 +1,13 @@
-import io
+import os
 import textwrap
 import requests
+import boto3
+import datetime
 
 from PIL import Image, ImageFont, ImageDraw
 from allauth.socialaccount.models import SocialAccount
 
+from django.conf import settings
 from ..models import Card
 
 """""""""""""""""""""""""""""""""""""""""""""""
@@ -13,10 +16,7 @@ TODO
   - とりあえず配置とか大きさとか折り返す文字数とかは適当
 - 背景画像
 - フォント
-- S3へのアップロード部分
 
-"""""""""""""""""""""""""""""""""""""""""""""""
-"""""""""""""""""""""""""""""""""""""""""""""""
 Pillowのメモ
 Image.new(): ベタ画像作成
 ImageDraw.Draw(): Drawオブジェクト作成
@@ -25,7 +25,9 @@ ImageDraw.text(): 文字列を画像に重ねる
 """""""""""""""""""""""""""""""""""""""""""""""
 
 
-# 背景画像作成
+##############
+# 背景画像作成 #
+##############
 def build_base_image():
     # 背景画像の作成（とりあえずオレンジ。既存画像を使う場合は以下）
     base_image = Image.new("RGB", (1200, 630), (255, 165, 0))
@@ -35,7 +37,9 @@ def build_base_image():
     return base_image
 
 
-# 文字入力用の関数定義（1行）
+################################
+# 画像への文字配置用の関数定義（1行） #
+################################
 def add_text_to_image_oneline(img, text, font_path, font_size, font_color, height, width, max_length=740):
     position = (width, height)
     font = ImageFont.truetype(font_path, font_size)
@@ -50,9 +54,10 @@ def add_text_to_image_oneline(img, text, font_path, font_size, font_color, heigh
     return img
 
 
-# 文字入力用の関数定義（複数行）
+##################################
+# 画像への文字配置用の関数定義（複数行） #
+##################################
 def add_text_to_image_multiline(img, text, font_path, font_size, font_color, height, width):
-    # position = (width, height)
     font = ImageFont.truetype(font_path, font_size)
     draw = ImageDraw.Draw(img)
     wrap_list = textwrap.wrap(text, 16)
@@ -62,11 +67,14 @@ def add_text_to_image_multiline(img, text, font_path, font_size, font_color, hei
         position = (width, y)
         draw.multiline_text(position, line, font_color, font=font)  # 1行分の文字列を画像に描画
         line_counter = line_counter + 1  # 行数のカウンターに1
+
     return img
 
 
-# アイコン貼り付け用の関数定義
-def add_twitter_icon(base_img, user):
+################################
+# 画像へのアイコン貼り付け用の関数定義 #
+################################
+def add_twitter_icon(img, user):
 # def add_twitter_icon(img):
     twitter_account_data = SocialAccount.objects.get(user_id=user.id)
     twitter_icon_url_normal = twitter_account_data.extra_data["profile_image_url_https"]
@@ -79,9 +87,40 @@ def add_twitter_icon(base_img, user):
     return img
 
 
-"""
-試し
-"""
+################################
+# S3へのアップロード #
+################################
+def upload_to_s3(img, card):
+
+    # S3バケットの取得
+    s3 = boto3.resource('s3')
+    bucket = s3.Bucket(settings.AWS_STORAGE_BUCKET_NAME)
+
+    # S3に保存するパスを設定
+    now = datetime.datetime.now()
+    file_name = card.auther.username + "_" + now.strftime('%Y%m%d_%H%M%S') + ".png"  # ファイル名
+    filepath_db = "test/" + file_name  # db保存用path
+    filepath_s3 = "media/" + filepath_db  # S3アップロード用path
+
+    # Imageオブジェクトを一時保存
+    file_path_local = "./media/" + file_name
+    img.save(file_path_local, "png")
+
+    # boto3にアップロードして、完了したら一時保存したものを削除
+    try:
+        bucket.upload_file(file_path_local, filepath_s3, ExtraArgs={"ContentType": "image/png"})  # S3に保存
+    finally:
+        os.remove(file_path_local)  # S3保存処理が終わったらローカルを削除
+
+    # DBのImageFieldを更新
+    card.card_image = filepath_db
+    card.save()
+
+    # S3のファイルパスを返す
+    return card.card_image.url
+
+
+"""試し（後で削除）"""
 base_image = build_base_image()
 title = "zoomで飲み会しようようホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲ"
 font_path = "./static/fonts/Koruri-Regular.ttf"  # TODO パスの入れ方？ フォント？
@@ -100,12 +139,3 @@ img = add_text_to_image_multiline(base_image, title, font_path, font_size, font_
 
 # アイコンを画像に貼る場合
 img = add_twitter_icon(base_image)
-
-
-"""
-画像の保存（→S3にアップロードするように書き換える必要あり）
-"""
-file_name = "sample.png"
-file_path = "./static/" + file_name
-
-img.save(file_path)
