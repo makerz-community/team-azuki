23258ef6dbce75c51c16d188348528b241a6a268
hide410 <hsuzuki410@gmail.com>
2020-05-25 15:03:09 +0900

    [途中]アイコン画像のマスク実装した

--- code/stayhome/libs/make_card.py
+++ code/stayhome/libs/make_card.py
@@ -1,10 +1,13 @@
+import io
 import os
 import textwrap
 import requests
 import boto3
 import datetime
 
-from PIL import Image, ImageFont, ImageDraw
+import unicodedata
+
+from PIL import Image, ImageFont, ImageDraw, ImageFilter
 from allauth.socialaccount.models import SocialAccount
 
 from django.conf import settings
@@ -23,6 +26,40 @@ ImageDraw.Draw(): Drawオブジェクト作成
 ImageDraw.textsize(): 文字列の(length, height)を返す
 ImageDraw.text(): 文字列を画像に重ねる
 """""""""""""""""""""""""""""""""""""""""""""""
+
+
+# #######################################
+# # 全角を2文字、半角を1文字でカウントする関数   #
+# # （add_text_to_image_multilineで使用） #
+# #######################################
+# def get_east_asian_width_count(text):
+#     count = 0
+#     for c in text:
+#         if unicodedata.east_asian_width(c) in 'FWA':
+#             count += 2
+#         else:
+#             count += 1
+#     return count
+
+
+#######################################
+# forループで最後の要素を取得する関数        #
+# （add_text_to_image_multilineで使用） #
+#######################################
+def lastone(iterable):
+    """与えられたイテレータブルオブジェクトの
+    最後の一つの要素の時にTrue、それ以外の時にFalseを返す
+    """
+    # イテレータを取得して最初の値を取得する
+    it = iter(iterable)
+    last = next(it)
+    # 2番目の値から開始して反復子を使い果たすまで実行
+    for val in it:
+        # 一つ前の値を返す
+        yield last, False
+        last = val  # 値の更新
+    # 最後の一つ
+    yield last, True
 
 
 ##############
@@ -52,50 +89,27 @@ def add_text_to_image_oneline(img, text, font_path, font_size, font_color, heigh
     return img
 
 
-#######################################
-# forループで最後の要素を取得する関数        #
-# （add_text_to_image_multilineで使用） #
-#######################################
-def lastone(iterable):
-    """与えられたイテレータブルオブジェクトの
-    最後の一つの要素の時にTrue、それ以外の時にFalseを返す
-    """
-    # イテレータを取得して最初の値を取得する
-    it = iter(iterable)
-    last = next(it)
-    # 2番目の値から開始して反復子を使い果たすまで実行
-    for val in it:
-        # 一つ前の値を返す
-        yield last, False
-        last = val  # 値の更新
-    # 最後の一つ
-    yield last, True
-
-
 ##################################
 # 画像への文字配置用の関数（複数行）    #
 ##################################
-def add_text_to_image_multiline(img, text, font_path, font_size, font_color, height, width):
+def add_text_to_image_multiline(img, text, font_path, font_size, font_color, height, width, line_height, one_line_length):
     font = ImageFont.truetype(font_path, font_size)
     draw = ImageDraw.Draw(img)
-    one_line_length = 16
     wrap_list = textwrap.wrap(text, one_line_length)
     line_counter = 0
-
     # wrap_listから最初の3行を1行づつ取り出し、lineに代入
     # （ただし最後の行だけis_lastがTrueになり、）
     for line, is_last in lastone(wrap_list[:3]):
         # 高さ座標をline_counterに応じて下げる
-        y = line_counter * 80 + height
+        y = line_counter * line_height + height
+        text_width, text_height = draw.textsize(line, font)
         position = (width, y)
-
         # 取り出したlineが3行目でかつ、4行目以降が存在する場合は3行目の末尾を「…」にする
         if is_last and wrap_list[3]:
             while len(line) >= one_line_length:
                 line = line[:-1]
             line = line + '…'
-
-        draw.multiline_text(position, line, font_color, font=font)  # 1行分の文字列を画像に描画
+        draw.multiline_text(position, line, font_color, font=font, align="center")  # 1行分の文字列を画像に描画
         line_counter = line_counter + 1  # 行数のカウンターに1
     return img
 
@@ -103,16 +117,24 @@ def add_text_to_image_multiline(img, text, font_path, font_size, font_color, hei
 ################################
 # 画像へのアイコン貼り付け用の関数定義 #
 ################################
-def add_twitter_icon(img, user):
-# def add_twitter_icon(img):
-    twitter_account_data = SocialAccount.objects.get(user_id=user.id)
-    twitter_icon_url_normal = twitter_account_data.extra_data["profile_image_url_https"]
-    # twitter_icon_url_normal = 'https://pbs.twimg.com/profile_images/1221399035887542272/1Mh2IfNL_normal.jpg'
-
+# def add_twitter_icon(img, user):
+def add_twitter_icon(img):
+    # twitter_account_data = SocialAccount.objects.get(user_id=user.id)
+    # twitter_icon_url_normal = twitter_account_data.extra_data["profile_image_url_https"]
+    twitter_icon_url_normal = 'https://pbs.twimg.com/profile_images/1221399035887542272/1Mh2IfNL_normal.jpg'
     twitter_icon_url_bigger = twitter_icon_url_normal.replace("_normal", "_bigger")  # サイズが小さいのでbiggerにreplace
-    icon = Image.open(io.BytesIO(requests.get(twitter_icon_url_bigger).content))  # TODO マスクかける
-    img.paste(icon, (80, 134))
-
+    icon = Image.open(io.BytesIO(requests.get(twitter_icon_url_bigger).content))
+    # 円形のマスク作成
+    mask = Image.new("L", icon.size)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, icon.size[0], icon.size[1]), fill=255)
+    mask = mask.filter(ImageFilter.GaussianBlur(1))
+    del draw
+    icon.putalpha(mask)
+    # なぜか一回保存しないと反映されないので一時保存してすぐ削除する
+    icon.save("./media/sample.png")
+    img.paste(icon, (600, 400), icon)
+    del icon
     return img
 
 
@@ -153,18 +175,29 @@ def upload_to_s3(img, card):
 base_image = build_base_image()
 title = "zoomで飲み会しようようホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲホゲ"
 font_path = "./static/fonts/Koruri-Regular.ttf"  # TODO パスの入れ方？ フォント？
-font_size = 57
+font_size = 90
 font_color = (255, 255, 255)
-height = 100
-width = 100
-# height = 155
-# width = 380
+height = 80
+width = 80
+line_height = 100
+one_line_length = 11
+
+
+"""by"""
+
+# 複数行のテキストを画像に貼る場合
+img = add_text_to_image_multiline(base_image, title, font_path, font_size, font_color, height, width, line_height, one_line_length)
+
 
 # 1行のテキストを画像に貼る場合
 img = add_text_to_image_oneline(base_image, title, font_path, font_size, font_color, height, width)
 
-# 複数行のテキストを画像に貼る場合
-img = add_text_to_image_multiline(base_image, title, font_path, font_size, font_color, height, width)
 
 # アイコンを画像に貼る場合
 img = add_twitter_icon(base_image)
+
+
+now = datetime.datetime.now()
+file_name = "username" + "_" + now.strftime('%Y%m%d_%H%M%S') + ".png"  # ファイル名
+file_path_local = "./media/" + file_name
+img.save(file_path_local, "png")
